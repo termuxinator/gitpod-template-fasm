cc75166729ff494a00e42d90a412d2e3156d8f65
termuxinator <39202840+termuxinator@users.noreply.github.com>
2023-07-25 20:31:07 -0500

    Update helloworld.s

--- helloworld.s
+++ helloworld.s
@@ -5,17 +5,16 @@ segment readable executable
 entry main
 
 main:
-    lea  rdi, [msg]                               ; address of msg goes into rdi
-    mov  rax, 14                                  ; put length of msg into rax
-    mov  rdx, rax                                 ; move rax to rdx
-    mov  rsi, rdi                                 ; move rdi to rsi
-    mov  rdi, 1                                   ; stdout
-    mov  rax, 1                                   ; sys_write
+    lea rsi, [msg]
+    mov rdx, msg_len
+    mov rdi, 1               ; stdout
+    mov rax, 1               ; sys_write
     syscall
-    xor  rdi, rdi                                 ; exit code 0
-    mov  rax, 60                                  ; sys_exit
+    xor rdi, rdi             ; exit code 0
+    mov rax, 60              ; sys_exit
     syscall
 
 segment readable writable
 
-msg  db 'Hello world!', 10, 0
+msg db 'Hello World!', 10
+msg_len equ $ - msg
